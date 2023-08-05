47217dccc9ec33e474049e4a6aebf399defdd2fb
Igor <igord902@gmail.com>
2023-08-05 14:28:55 -0300

    react navigation

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,16 +1,19 @@
 import React from 'react'
-import {View,Text, TouchableOpacity} from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import { Feather } from '@expo/vector-icons';
 import styles from './styles';
 
-export default function Header(){
-    return(
-        <View style = {styles.viewHeader}>
-         <TouchableOpacity> 
-        <Feather style={{marginLeft:-72}} name="menu" size={36} color="white" />
-        </TouchableOpacity>  
-        <Text style = {styles.textHeader}>  TEC FILMES</Text> 
-
+export default function Header() {
+    return ( 
+        <View style = { styles.viewHeader }>
+        <TouchableOpacity>
+        <Feather style = {
+            { marginLeft: -72 } }
+        name = "menu"
+        size = { 36 }
+        color = "white" />
+        </TouchableOpacity>
+        <Text style = { styles.textHeader } > TCC FILMES </Text> 
         </View>
     );
 }
