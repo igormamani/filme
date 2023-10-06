a3e55cb2dbab26b0b13f7a4eaad80548d6e46321
Igor <igord902@gmail.com>
2023-10-06 17:21:08 -0300

    api ´publica

--- src/screan/Home/index.js
+++ src/screan/Home/index.js
@@ -3,7 +3,8 @@ import { FlatList, StyleSheet, Text, View } from 'react-native';
 import Header from '../../../src/components/header';
 import Search from '../../../src/components/searchBar';
 import Banner from '../../../src/components/bannerMovies';
-import Filmes from '../../../src/data/filmes';
+// import Filmes from '../../../src/data/filmes';
+import React,{useState, useEffect} from 'react';
 import Series from '../../../src/data/series';
 import CardFilmes from '../../../src/components/cardFilmes';
 import CardSeries from '../../../src/components/cardSeries';
@@ -11,6 +12,28 @@ import CardSeries from '../../../src/components/cardSeries';
 
 
 export default function App() {
+
+
+const [movies, setMovies] = useState([]);
+
+useEffect(() => {
+
+  async function buscarFilmes(){
+
+
+    const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=e321d32f7c82226ef8c44d9a8af35fb5&language=pt-BR');
+    const data = await response.json();
+
+    console.log(data.results);
+    setMovies(data.results);
+  } 
+
+  buscarFilmes();
+
+
+
+}, [])
+
   return (
     <View style={styles.container}>
       <Header></Header>
@@ -22,7 +45,7 @@ export default function App() {
     <FlatList 
     showsHorizontalScrollIndicator = {false}
     horizontal ={true}
-    data  ={Filmes} 
+    data  ={''} 
     keyExtractor = {(item) => item.id}
     renderItem = { ({item})  =>(
 
